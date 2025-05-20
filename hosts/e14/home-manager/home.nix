cbc72ea09c525f64933fab6d2b3da92ca971b5b8
corgijan <jan.vaorin@tu-dortmund.de>
2025-05-20 02:45:48 +0200

    updates

---
+++ hosts/e14/home-manager/home.nix
@@ -0,0 +1,150 @@
+# This is your home-manager configuration file
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+  ];
+
+   home.sessionVariables = {
+   	EDITOR = "nvim";
+   };
+
+   home.file = { 
+   ".config/fish/config.fish" = { text = (builtins.readFile ../../../configs/fish/config.fish); executable = false; };
+   ".config/ghostty/config" = { text = (builtins.readFile ../../../configs/ghostty/config); executable = false; };
+    # Nvim
+   #".config/nvim/init.lua" = { text = (builtins.readFile ../../../configs/nvim/init.lua); executable = false; };
+   ".config/nvim" = { 
+    source = ../../../configs/nvim-complete/nvim;
+    recursive = true;
+   };
+   };
+
+   dconf.settings = {
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+        picture-uri = "file:///home/jan/nix/images/yosemite.jpg";
+        picture-uri-dark = "file:///home/jan/nix/images/yosemite.jpg";
+      };
+
+ 	"org/gnome/desktop/wm/preferences" = {
+      		button-layout = "close:";
+    	};
+	
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      # `gnome-extensions list` for a list
+      enabled-extensions = [
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+	      "fullscreen-to-empty-workspace2@corgijan.dev"
+	      "caffeine@patapon.info"
+	      "blur-my-shell@aunetx"
+	      "clipboard-history@alexsaveau.dev"
+      ];
+    };
+
+    "org/gnome/shell" = {
+      favorite-apps = [
+	      "app.zen_browser.zen.desktop"
+        "org.gnome.Nautilus.desktop"
+	      "com.mitchellh.ghostty.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+      enable-animations = false; 
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = true;
+    };
+    };
+
+      gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  # TODO: Set your username
+  home = {
+    username = "jan";
+    homeDirectory = "/home/jan";
+  };
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  # home.packages = with pkgs; [ steam ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+    programs.git = {
+    enable = true;
+    userName = "corgijan";
+    userEmail = "jan.vaorin@tu-dortmund.de";
+    aliases = {
+      undo = "reset HEAD~1 --mixed";
+      amend = "commit -a --amend";
+    };
+    extraConfig = {
+      init.defaultBranch = "development";
+      pull = {
+        rebase = true;
+      };
+      branch = {
+        autosetupmerge = true;
+      };
+    };
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "25.05";
+}
+
---
+++ hosts/e14/nixos/configuration.nix
@@ -0,0 +1,164 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, pkgs-unstable, system ? builtins.currentSystem,  ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hw.nix
+      ./bootloader.nix
+      ../../../modules/packages.nix
+      ../../../modules/desktop_packages.nix
+    ];
+
+  basepkgs = {
+    enable = true;
+  };
+
+  desktoppkgs = {
+    enable = true;
+  };
+
+
+ environment.systemPackages = with pkgs-unstable; [
+    htop # Example package from unstable branch
+    k9s
+    jetbrains.rust-rover
+    jetbrains.idea-ultimate
+    (pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.pycharm-professional ["github-copilot"])
+    spotify
+    jetbrains-mono
+    #zed-editor-fhs
+  ];
+  programs.nix-ld.enable = true;
+
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  users.groups.libvirtd.members = ["jan"];
+
+  services.flatpak.enable = true;
+  services.tailscale.enable = true;
+
+  programs.virt-manager.enable = true;
+  programs.fish.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  virtualisation.docker.enable = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+    desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverridePackages = [ pkgs.mutter ];
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['scale-monitor-framebuffer','xwayland-native-scaling']
+      '';
+    };
+  };
+
+
+  # Enable the GNOME Desktop Environment.
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jan = {
+    isNormalUser = true;
+    description = "jan";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  users.extraGroups.docker.members = [ "jan" ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  system.stateVersion = "24.11";
+}
